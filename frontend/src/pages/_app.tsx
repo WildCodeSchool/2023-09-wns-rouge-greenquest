67850886edcddf77488725c98a7bb15a074bc59d
Samuel Faber <samuel.faber@live.fr>
2024-02-06 16:39:54 +0100

    ✨ Ajouter des fichiers de police Roboto

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,3 +1,7 @@
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 import type { AppProps } from "next/app";
 import dynamic from "next/dynamic";
 
