ddf82919813746af9113fe005d21402681187cae
Samuel Faber <samuel.faber@live.fr>
2024-02-06 16:36:30 +0100

    ✨ Ajouter la page de tunnel de quête

---
+++ frontend/src/pages/questtunnel/index.tsx
@@ -0,0 +1,12 @@
+import Layout from "@/components/Layout";
+
+export default function QuestTunnel() {
+  return (
+    <>
+      <Layout title="Création de quête">
+        <h1>Coucou GreenQuest!</h1>
+        <p>Ceci est notre tunnel</p>
+      </Layout>
+    </>
+  );
+}
