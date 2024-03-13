fbcc333c5561f7904937fdc5d28f2d6d19f0a472
LAURE JANIN <ljanin51@gmail.com>
2024-03-13 10:59:53 +0100

    🧪 test ci

--- backend/_tests/quests.spec.ts
+++ backend/_tests/quests.spec.ts
@@ -13,11 +13,11 @@ let schema: GraphQLSchema;
 beforeAll(async () => {
   dataSource = new DataSource({
     ...dataSourceOptions,
-    host: process.env.POSTGRES_HOST || "127.0.0.1",
-    port: parseInt(process.env.POSTGRES_PORT || "5432"),
-    username: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DB,
+    host: "127.0.0.1",
+    port: 5571,
+    username: "postgres",
+    password: "pgpassword",
+    database: "postgres",
     dropSchema: true,
     logging: false,
   });
