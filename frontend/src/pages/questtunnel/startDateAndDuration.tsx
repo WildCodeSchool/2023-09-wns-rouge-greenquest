bc6a6ac29f6ba1a6a0fc41ffbbd9578461a13532
Laure Janin <laure.janin@bedrockstreaming.com>
2024-03-28 15:00:17 +0100

    💄 fields validation

--- frontend/src/pages/questtunnel/startDateAndDuration.tsx
+++ frontend/src/pages/questtunnel/startDateAndDuration.tsx
@@ -2,18 +2,18 @@ import { useState } from "react";
 import { useRouter } from "next/router";
 import Layout from "@/components/Layout";
 import { Grid, Typography, TextField, Button, Paper } from "@mui/material";
+import { useQuestContext } from "@/contexts/QuestContext";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
-import { useQuestContext } from "@/contexts/QuestContext";
 import dayjs, { Dayjs } from "dayjs";
+import "dayjs/locale/fr";
 
 export default function StartAndDuration() {
   const router = useRouter();
-  const [startDate, setStartDate] = useState<Dayjs | null>(dayjs("MM-DD-YYYY"));
+  const [startDate, setStartDate] = useState<Dayjs | null>(null);
   const [duration, setDuration] = useState<number>(0);
   const { setQuestInfo } = useQuestContext();
-  const { questInfo } = useQuestContext();
 
   const previousPage = () => {
     router.back();
@@ -22,7 +22,6 @@ export default function StartAndDuration() {
   const nextPage = () => {
     // Convertir la date de début en Dayjs avant de la définir
     const formattedStartDate = startDate?.toISOString();
-    console.log(formattedStartDate);
 
     setQuestInfo((prevQuestInfo) => ({
       ...prevQuestInfo,
@@ -64,12 +63,14 @@ export default function StartAndDuration() {
           <Typography variant="h2" sx={{ fontSize: "1.5rem" }}>
             Étape 2
           </Typography>
-          <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="fr">
             <DatePicker
               label="Date de début de la quête"
+              format="DD/MM/YYYY"
               value={startDate}
               onChange={(newValue) => setStartDate(newValue)}
               sx={{ width: "100%" }}
+              minDate={dayjs()}
             />
           </LocalizationProvider>
           <TextField
@@ -99,6 +100,7 @@ export default function StartAndDuration() {
               variant="contained"
               sx={{ bgcolor: "#7BD389", color: "#000000" }}
               onClick={nextPage}
+              disabled={!startDate || duration < 0}
             >
               Suivant
             </Button>
